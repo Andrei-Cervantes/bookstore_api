ca04dddde9765bc0216ddfeeb1032c12f0fbaf8b
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-07-31 23:36:07 +0800

    add require admin middleware for admin routes and add get all users

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,20 @@ import { User } from "../models/userModel.js";
 import { successResponse, errorResponse } from "../utils/formatResponse.js";
 
 const usersController = () => {
-  const getAllUsers = async (req, res) => {};
+  const getAllUsers = async (req, res) => {
+    try {
+      // get all users
+      const users = await User.find({});
+
+      if (!users || users.length === 0) {
+        return errorResponse(res, 404, "No users found");
+      }
+
+      return successResponse(res, 200, "Users fetched successfully", { users });
+    } catch (error) {
+      return errorResponse(res, 500, error.message);
+    }
+  };
 
   const getSingleUser = async (req, res) => {};
 
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersController from "../controllers/adminController.js";
+import { requireAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -8,15 +9,15 @@ const { getAllUsers, getSingleUser, setUserRole, deleteUser } =
   usersController();
 
 // Admin Route for getting all users
-router.get("/users", getAllUsers);
+router.get("/users", requireAdmin, getAllUsers);
 
 // Admin Route for getting a single user
-router.get("/users/:id", getSingleUser);
+router.get("/users/:id", requireAdmin, getSingleUser);
 
 // Admin Route for setting user role (user, librarian, admin)
-router.put("/users/:id/role", setUserRole);
+router.put("/users/:id/role", requireAdmin, setUserRole);
 
 // Admin Route for deleting a user
-router.delete("/users/:id", deleteUser);
+router.delete("/users/:id", requireAdmin, deleteUser);
 
 export default router;
