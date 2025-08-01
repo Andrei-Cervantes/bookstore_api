8a7316e1651771cef745b456f5572a4d921c92f5
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-08-01 15:35:48 +0800

    add return borrow request controller

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -56,7 +56,43 @@ const borrowController = () => {
     }
   };
 
-  const returnBorrowRequest = async (req, res) => {};
+  const returnBorrowRequest = async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const borrowRequest = await BorrowRequest.findById(id);
+      if (!borrowRequest)
+        return errorResponse(res, 404, "Borrow request not found");
+
+      const isOwner = borrowRequest.user.toString() === req.user._id.toString();
+      const isLibrarian = req.user.role === "librarian";
+
+      if (!isOwner && !isLibrarian)
+        return errorResponse(
+          res,
+          403,
+          "You are not authorized to return this book"
+        );
+
+      if (borrowRequest.status !== "approved")
+        return errorResponse(
+          res,
+          400,
+          "Borrow request is not approved or returned"
+        );
+
+      borrowRequest.status = "returned";
+      borrowRequest.returnedAt = new Date();
+      await borrowRequest.save();
+
+      return successResponse(res, 200, "Borrowed book returned successfully", {
+        borrowRequest,
+      });
+    } catch (error) {
+      console.log(error.message);
+      return errorResponse(res, 500, "Internal server error");
+    }
+  };
 
   const getAllBorrowRequests = async (req, res) => {};
 
